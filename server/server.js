2e8e3575761bd18ce33a6aa81598c6fde22479f3
DEZester <aromudez@gmail.com>
2023-08-24 21:07:15 +0300

    Connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import cors from 'cors'
-
 const app = express()
+dotenv.config()
 
-app.listen(3001, () => console.log(`Server is started`))
+const PORT = process.env.PORT || 3001
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => { app.listen(PORT, () => console.log(`Server is started with port:${process.env.PORT}`))})
+  .catch((err) => console.log(`${err} did not connect`))
+
